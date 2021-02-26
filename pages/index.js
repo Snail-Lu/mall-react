e9fe01fdc3383dd283fc15d39dd4e8c4fd1c1d71
Snail-Lu <snaillu@yeah.net>
2021-02-26 21:46:46 +0800

    feat: 首页增加搜索栏和轮播

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,60 @@
 import Head from 'next/head';
-import React from 'react';
+import React, { Component } from 'react';
 import styles from '../styles/Home.module.css';
-import { Button } from 'antd-mobile';
+import { SearchBar, Carousel } from 'antd-mobile';
 import Layout from 'components/layout';
 
-export default function Home() {
-  return (
-    <Layout pathname="/" wrapClass="home" title="首页">
-      <Button type="primary" inline>首页</Button>
-    </Layout>
-  )
+export default class Home extends Component {
+  state = {
+    data: ['1', '2', '3'],
+    imgHeight: 176,
+  }
+
+  componentDidMount() {
+    // simulate img loading
+    setTimeout(() => {
+      this.setState({
+        data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+      });
+    }, 100);
+  }
+
+  render() {
+    return (
+      <Layout pathname="/" wrapClass="home" title="首页">
+        <SearchBar placeholder="Search" maxLength={8} />
+        <Carousel className={styles.space_carousel}
+          autoplay
+          infinite
+          beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+          afterChange={index => this.setState({ slideIndex: index })}
+          >
+            {this.state.data.map((val, index) => (
+              <a
+                key={val}
+                href="http://www.alipay.com"
+                style={{
+                  display: 'block',
+                  position: 'relative',
+                  top: this.state.slideIndex === index ? -10 : 0,
+                  height: this.state.imgHeight,
+                  boxShadow: '2px 1px 1px rgba(0, 0, 0, 0.2)',
+                }}
+              >
+                <img
+                  src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                  alt=""
+                  style={{ width: '100%', verticalAlign: 'top' }}
+                  onLoad={() => {
+                    // fire window resize event to change height
+                    window.dispatchEvent(new Event('resize'));
+                    this.setState({ imgHeight: 'auto' });
+                  }}
+                />
+              </a>
+            ))}
+          </Carousel>
+      </Layout>
+    )
+  }
 }
