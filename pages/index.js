985d076a447c82722d537eef655059704e0179ae
Snail-Lu <snaillu@yeah.net>
2021-03-01 23:13:21 +0800

    feat: 首页轮播bug修复及增加长列表

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,125 @@
 import Head from 'next/head';
 import React, { Component } from 'react';
 import styles from '../styles/Home.module.css';
-import { SearchBar, Carousel } from 'antd-mobile';
+import { SearchBar, Carousel, ListView  } from 'antd-mobile';
 import Layout from 'components/layout';
+const data = [
+  {
+    img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
+    title: 'Meet hotel',
+    des: '不是所有的兼职汪都需要风吹日晒',
+  },
+  {
+    img: 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png',
+    title: 'McDonald\'s invites you',
+    des: '不是所有的兼职汪都需要风吹日晒',
+  },
+  {
+    img: 'https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png',
+    title: 'Eat the week',
+    des: '不是所有的兼职汪都需要风吹日晒',
+  },
+];
+const NUM_ROWS = 20;
+let pageIndex = 0;
+
+function genData(pIndex = 0) {
+  const dataBlob = {};
+  for (let i = 0; i < NUM_ROWS; i++) {
+    const ii = (pIndex * NUM_ROWS) + i;
+    dataBlob[`${ii}`] = `row - ${ii}`;
+  }
+  return dataBlob;
+}
 
 export default class Home extends Component {
-  state = {
-    data: ['1', '2', '3'],
-    imgHeight: 176,
+  constructor(props) {
+    super(props);
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
+    });
+
+    this.state = {
+      carouselList: ['1', '2', '3'],
+      imgHeight: 176,
+      dataSource,
+      isLoading: true,
+    };
   }
+
 
   componentDidMount() {
     // simulate img loading
     setTimeout(() => {
       this.setState({
-        data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+        carouselList: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
       });
     }, 100);
+
+    setTimeout(() => {
+      this.rData = genData();
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(this.rData),
+        isLoading: false,
+      });
+    }, 600);
+  }
+
+  onEndReached = (event) => {
+    // load new data
+    // hasMore: from backend data, indicates whether it is the last page, here is false
+    if (this.state.isLoading && !this.state.hasMore) {
+      return;
+    }
+    console.log('reach end', event);
+    this.setState({ isLoading: true });
+    setTimeout(() => {
+      this.rData = { ...this.rData, ...genData(++pageIndex) };
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(this.rData),
+        isLoading: false,
+      });
+    }, 1000);
   }
 
   render() {
+    const separator = (sectionID, rowID) => (
+      <div
+        key={`${sectionID}-${rowID}`}
+        style={{
+          backgroundColor: '#F5F5F9',
+          height: 8,
+          borderTop: '1px solid #ECECED',
+          borderBottom: '1px solid #ECECED',
+        }}
+      />
+    );
+    let index = data.length - 1;
+    const row = (rowData, sectionID, rowID) => {
+      if (index < 0) {
+        index = data.length - 1;
+      }
+      const obj = data[index--];
+      return (
+        <div key={rowID} style={{ padding: '0 15px' }}>
+          <div
+            style={{
+              lineHeight: '50px',
+              color: '#888',
+              fontSize: 18,
+              borderBottom: '1px solid #F6F6F6',
+            }}
+          >{obj.title}</div>
+          <div style={{ display: '-webkit-box', display: 'flex', padding: '15px 0' }}>
+            <img style={{ height: '64px', marginRight: '15px' }} src={obj.img} alt="" />
+            <div style={{ lineHeight: 1 }}>
+              <div style={{ marginBottom: '8px', fontWeight: 'bold' }}>{obj.des}</div>
+              <div><span style={{ fontSize: '30px', color: '#FF6E27' }}>{rowID}</span>¥</div>
+            </div>
+          </div>
+        </div>
+      );
+    };
     return (
       <Layout pathname="/" wrapClass="home" title="首页">
         <SearchBar placeholder="Search" maxLength={8} />
@@ -29,7 +129,7 @@ export default class Home extends Component {
           beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
           afterChange={index => this.setState({ slideIndex: index })}
           >
-            {this.state.data.map((val, index) => (
+            {this.state.carouselList.map((val, index) => (
               <a
                 key={val}
                 href="http://www.alipay.com"
@@ -37,23 +137,35 @@ export default class Home extends Component {
                   display: 'block',
                   position: 'relative',
                   top: this.state.slideIndex === index ? -10 : 0,
-                  height: this.state.imgHeight,
                   boxShadow: '2px 1px 1px rgba(0, 0, 0, 0.2)',
+                  height: this.state.imgHeight
                 }}
               >
                 <img
                   src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
                   alt=""
                   style={{ width: '100%', verticalAlign: 'top' }}
-                  onLoad={() => {
-                    // fire window resize event to change height
-                    window.dispatchEvent(new Event('resize'));
-                    this.setState({ imgHeight: 'auto' });
-                  }}
                 />
               </a>
             ))}
-          </Carousel>
+        </Carousel>
+        <ListView
+          ref={el => this.lv = el}
+          dataSource={this.state.dataSource}
+          renderHeader={() => <span>header</span>}
+          renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+            {this.state.isLoading ? 'Loading...' : 'Loaded'}
+          </div>)}
+          renderRow={row}
+          renderSeparator={separator}
+          className="am-list"
+          pageSize={4}
+          useBodyScroll
+          onScroll={() => { console.log('scroll'); }}
+          scrollRenderAheadDistance={500}
+          onEndReached={this.onEndReached}
+          onEndReachedThreshold={10}
+        />
       </Layout>
     )
   }
