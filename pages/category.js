c6086219e7f9d489672bc64b00a471b841c7b421
Snail-Lu <snaillu@yeah.net>
2021-03-02 22:58:26 +0800

    feat: 分类页页面样式完成

--- pages/category.js
+++ pages/category.js
@@ -1,12 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from '../styles/Category.module.css';
-import { Button } from 'antd-mobile';
+import { Menu } from 'antd-mobile';
 import Layout from 'components/layout';
 
-export default function Category() {
-  return (
-    <Layout pathname="/category" wrapClass="category" title="分类页">
-      <Button type="primary" inline>分类页</Button>
-    </Layout>
-  )
+export default class Category extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initData: [
+        {
+          value: '1',
+          label: 'Food',
+          children: [
+            {
+              label: 'All Foods',
+              value: '1',
+              disabled: false,
+            },
+            {
+              label: 'Chinese Food',
+              value: '2',
+            }, {
+              label: 'Hot Pot',
+              value: '3',
+            }, {
+              label: 'Buffet',
+              value: '4',
+            }, {
+              label: 'Fast Food',
+              value: '5',
+            }, {
+              label: 'Snack',
+              value: '6',
+            }, {
+              label: 'Bread',
+              value: '7',
+            }, {
+              label: 'Fruit',
+              value: '8',
+            }, {
+              label: 'Noodle',
+              value: '9',
+            }, {
+              label: 'Leisure Food',
+              value: '10',
+            }],
+        }, {
+          value: '2',
+          label: 'Supermarket',
+          children: [
+            {
+              label: 'All Supermarkets',
+              value: '1',
+            }, {
+              label: 'Supermarket',
+              value: '2',
+              disabled: true,
+            }, {
+              label: 'C-Store',
+              value: '3',
+            }, {
+              label: 'Personal Care',
+              value: '4',
+            }],
+        },
+        {
+          value: '3',
+          label: 'Extra',
+          isLeaf: true,
+          children: [
+            {
+              label: 'you can not see',
+              value: '1',
+            },
+          ],
+        },
+      ],
+    };
+  }
+  render() {
+    const { initData } = this.state;
+    return (
+      <Layout pathname="/category" wrapClass="category" title="分类页">
+        <Menu
+          className="foo-menu"
+          data={initData}
+          value={['1', '3']}
+          onChange={this.onChange}
+          height={600}
+        />
+      </Layout>
+    )
+  }
 }
